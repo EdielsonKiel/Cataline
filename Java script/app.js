91e6e65880a3079b81bec35a5fa5b7a462bd0265
Edielson Kiel <edielson.kiel.dev@gmail.com>
2023-01-02 22:14:07 -0300

    02/01/2023 inucui de estudo de concatenação, operadores de atribuição, funções, condicionais e operadores de comparação

--- Java script/app.js
+++ Java script/app.js
@@ -1,1 +1,15 @@
 console.log('Hello cataline')
+
+//Concatenação o sinal de concatenação é +
+var product = 'camisa'
+var price = 10
+
+console.log('O preço de ' + product + ' é R$ ' + price)
+
+//operadores aritiméticos
+
+var x = 10,y = 3
+
+var result = x + y
+
+console.log( result )
