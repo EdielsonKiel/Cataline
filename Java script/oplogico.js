ed0d34b974a7d73338a5c667f83592528ff2c38d
Edielson Kiel <edielson.kiel.dev@gmail.com>
2023-01-03 23:49:04 -0300

    Uso de operadores logicos

---
+++ Java script/oplogico.js
@@ -0,0 +1,20 @@
+// AND = &&, OR ||, NOT !
+
+var email = 'edielsonkiel@gmail.com'
+var password = 'abobora'
+
+if (email === 'edielsonkiel@gmail.com' && password ==='abobora'){
+    console.log('usuario valido')
+} else {
+    console.log('usuario invalido')
+}
+
+var idade = 18
+var sex = 'M'
+
+if(idade >= 18 || sex === 'M') {
+    console.log ('entrada permitida')
+}
+else {
+    console.log ('entrada proibida')
+}
